4a14dd15fc904d5ffbead614e911642ce3f7f900
asserelzeki6 <asserelzeki@gmail.com>
2024-11-30 04:41:30 +0200

    circular import error fixed

--- server/utils/tree.py
+++ server/utils/tree.py
@@ -4,8 +4,8 @@ import os
 # Add the project's root directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from server.utils.Node import Node
-from server.utils.scoring import *
+from utils.Node import Node
+from utils.scoring import *
 import copy
 
 empty = '.'
